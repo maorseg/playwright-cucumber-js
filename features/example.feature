8bb4b3cf0c7c0b082a134924a1966ce25374619e
Maor <maorseg@gmail.com>
2025-08-04 00:55:08 +0300

    updated files

--- features/example.feature
+++ features/example.feature
@@ -1,3 +1,4 @@
 Feature: Example test
   Scenario: Open homepage
     Given I open the homepage
+    Then I verify homepage title
--- support/world.js
+++ support/world.js
@@ -1,15 +1,8 @@
 const { setWorldConstructor } = require('@cucumber/cucumber');
-const { chromium } = require('playwright');
 
 class CustomWorld {
-  async init() {
-    this.browser = await chromium.launch();
-    this.page = await this.browser.newPage();
-  }
-
-  async close() {
-    await this.page.close();
-    await this.browser.close();
+  constructor() {
+    this.page = null;
   }
 }
 
