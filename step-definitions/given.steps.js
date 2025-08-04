25301d1bff0f81673c2e30b9bb8797bfe09fb61e
Maor <maorseg@gmail.com>
2025-08-05 01:05:47 +0300

    refactored

--- step-definitions/given.steps.js
+++ step-definitions/given.steps.js
@@ -1,14 +1,14 @@
 const { Given } = require('@cucumber/cucumber');
 const { chromium } = require('playwright');
 const { setDefaultTimeout } = require('@cucumber/cucumber');
-const dataTest = require('./dataTest.json');
+const testData = require('./testData.json');
 setDefaultTimeout(30 * 1000);
 
 Given('I open the homepage', async function () {
   const browser = await chromium.launch();
   const context = await browser.newContext();
   const page = await context.newPage();
-  await page.goto(dataTest.homepageURL);
+  await page.goto(testData.homepageURL);
   this.page = page;
 });
 
--- step-definitions/then.steps.js
+++ step-definitions/then.steps.js
@@ -1,10 +1,10 @@
 const { Then } = require('@cucumber/cucumber');
 const { expect } = require('@playwright/test');
-const dataTest = require('./dataTest.json');
+const testData = require('./testData.json');
 
 Then('I verify homepage shown', async function () {
   const title = await this.page.title();
-  expect(title).toBe(dataTest.homepageTitle);
+  expect(title).toBe(testData.homepageTitle);
 });
 
 
